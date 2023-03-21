99df459cc95a720fe1c461e448f312f681965dd2
OppKunG <67791331+OppKunG@users.noreply.github.com>
2023-03-21 14:55:20 +0700

    Unit Test Demo

---
+++ testing-master/src/main/java/com/king/mongkut/testing/controller/CalculationController.java
@@ -0,0 +1,67 @@
+package com.king.mongkut.testing.controller;
+
+import com.king.mongkut.testing.service.CalculationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CalculationController {
+
+    private final CalculationService calculationService;
+
+    @Autowired
+    public CalculationController(CalculationService calculationService) {
+        this.calculationService = calculationService;
+    }
+
+    @GetMapping(value = "/plus/{number1}/{number2}")
+    public ResponseEntity<?> plus(@PathVariable("number1") String number1, @PathVariable("number2") String number2) {
+        {
+            try {
+                int result = calculationService.plus(number1, number2);
+                return ResponseEntity.ok(result);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                return ResponseEntity.badRequest().body(ex.getMessage());
+            }
+        }
+    }
+
+    @GetMapping("/minus")
+    public ResponseEntity<?> minus(@RequestParam String number1, @RequestParam String number2) {
+        {
+            int minus = calculationService.minus(number1, number2);
+            if (minus != -1) {
+                return ResponseEntity.ok(minus);
+            } else {
+                return ResponseEntity.badRequest().body("something went wrong");
+            }
+        }
+    }
+
+    @GetMapping(value = "/multiply/{number1}/{number2}")
+    public ResponseEntity<?> multiply(@PathVariable("number1") String number1, @PathVariable("number2") String number2) {
+        {
+            try {
+                int result = calculationService.multiply(number1, number2);
+                return ResponseEntity.ok(result);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                return ResponseEntity.badRequest().body(ex.getMessage());
+            }
+        }
+    }
+
+    @GetMapping("/divide")
+    public ResponseEntity<?> divide(@RequestParam String number1, @RequestParam String number2) {
+        {
+            int divide = calculationService.divide(number1, number2);
+            if (divide != -1) {
+                return ResponseEntity.ok(divide);
+            } else {
+                return ResponseEntity.badRequest().body("something went wrong");
+            }
+        }
+    }
+}
